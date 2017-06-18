9bfa07c9f024909c8f6164a356c24a499d34213e
Yuxiang Dai <yuxiang.dai43@gmail.com>
2017-06-17 22:35:55 -0400

    integrating react router

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -3,7 +3,8 @@
 // 2. copy of current state
 
 function comments(state = [], action){
-
+    console.log(state, action);
+    return state;
 }
 
 export default comments;
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,7 +3,8 @@
 // 2. copy of current state
 
 function posts(state = [], action){
-
+    console.log(state, action);
+    return state;
 }
 
 export default posts;
