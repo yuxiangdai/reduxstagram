0d0fec80f6fc2afc3ba6ced556dea9aabbbf79ec
Yuxiang Dai <yuxiang.dai43@gmail.com>
2017-06-19 22:55:22 -0400

    pull state into components

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
     render() {
         return(
             <div className="photo-grid">
-                test
+                {this.props.posts.map((post, i) => <Photo {...
+                    this.props} key={i} i={i} post={post} />)}
             </div>
         )
     }
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -16,7 +16,7 @@ import store, { history } from './store';
 const router = (
     <Provider store={store}>
         <Router history={history}>
-            <Route path="/" component={Main}>
+            <Route path="/" component={App}>
                 <IndexRoute component={PhotoGrid}></IndexRoute>
                 <Route path="/view/:postId" component={Single}></Route>
             </Route>
