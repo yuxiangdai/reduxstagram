c861e62555e9458ef8327cdb67551a9d3800b89d
Yuxiang Dai <yuxiang.dai43@gmail.com>
2017-06-22 21:47:28 -0400

    single photo + comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+//import comments
 
 
 const Single = React.createClass({
     render() {
+        const {postId} = this.props.params.postId;
+        const i = this.props.posts.findIndex((post) => post.code === postId );
+        const post = this.props.posts[i];
+        const postComments = this.props.comments[postId] || [];
+
         return(
             <div className="single-photo">
-                
+                <Photo i={i} post={post} {...this.props} />
+                <Comments postsComments={postComments}/>
             </div>
         )
     }
