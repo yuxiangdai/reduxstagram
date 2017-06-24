963b455409c2ae5ab7188aa6c0561b798d44a98b
Yuxiang Dai <yuxiang.dai43@gmail.com>
2017-06-23 22:43:11 -0400

    comment CRUD

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,8 +1,27 @@
-// reducer:
-// 1. action
-// 2. copy of current state
+function postComments(state = [], action){
+    switch(action.type){
+        case 'ADD_COMMENT':
+            return [...state,{
+                user: action.author,
+                text: action.comment
+            }];
+        case 'REMOVE_COMMENT':
+            return [
+                ...state.slice(0, action.i),
+                ...state.slice(action.i + 1)
+            ]
+        default:
+            return state;
+    }
+}
 
 function comments(state = [], action){
+    if(typeof action.postId !== 'undefined'){
+        return {
+            ...state,
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
     return state;
 }
 
